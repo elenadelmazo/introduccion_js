888c8342817d4fe1ea358149352c5c1066b2544a
Elena del Mazo <delmazobermejo@gmail.com>
2022-09-14 15:05:54 +0200

    añado desafío completado

---
+++ desafio.js
@@ -0,0 +1,10 @@
+/* Crea una lista con los siguientes elementos:
+
+- Tu nombre (string)
+- Tu apellido (string)
+- Tu edad (number)
+- ¿Eres desarrollador de aplicaciones web? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+- Tu pasatiempos favoritos (Array)
+- Declara una funcion miActividadFavorita la cual retorne un string con la accion que mas te guste realizar */
